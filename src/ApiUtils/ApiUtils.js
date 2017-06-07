54d6f918fe5bb1132f8c5388cdda0a54a0bfa837
PeiDa Kuo <guo.peida@gmail.com>
2017-06-07 04:40:44 -0700

    shorter syntax for json request

--- src/ApiUtils/ApiUtils.js
+++ src/ApiUtils/ApiUtils.js
@@ -3,16 +3,11 @@ import StockActions from '../actions/StockActions';
 
 const ApiUtils = {
   fetchStockPrices(symb) {
-    d3.request('http://localhost:8888/stockdata/'+symb)
-      .mimeType("application/json")
-      .response(function(xhr) { return JSON.parse(xhr.responseText); })
-      .get(function (resp) {
+    d3.json('http://localhost:8888/stockdata/'+symb, function (resp) {
         if (resp && resp.status.code === 200) {
           StockActions.getStockPrices(resp.results);
-        } else {
-          StockActions.getStockPrices(null);
         }
-      })
+    })
   }
 }
 
