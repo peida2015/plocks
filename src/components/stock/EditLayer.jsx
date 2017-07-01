89b6771d15bd6d47afc2cd7fab80e1ec38a7d811
PeiDa Kuo <guo.peida@gmail.com>
2017-06-30 20:10:28 -0700

    allows points to be dragged and moved

--- src/components/stock/EditLayer.jsx
+++ src/components/stock/EditLayer.jsx
@@ -6,10 +6,13 @@ class EditLayer extends Component {
     super();
 
     this.buildLines = this.buildLines.bind(this);
-    this.drawLineHandler = this.drawLineHandler.bind(this);
+    this.setPointHandler = this.setPointHandler.bind(this);
     this.freeDrawingHandler = this.freeDrawingHandler.bind(this);
+    this.toggleClickedCircle = this.toggleClickedCircle.bind(this);
+    this.circleUpdatePosition = this.circleUpdatePosition.bind(this);
 
     this.state = {
+      clickedPointIndex: null,
       lines: List(),
       freeDrawing: List(),
       lastEdit: List()
@@ -20,7 +23,7 @@ class EditLayer extends Component {
     var editDetectArea = document.getElementById("editDetectArea");
     switch (this.props.editControls) {
       case "drawLine":
-        editDetectArea.addEventListener("click", this.drawLineHandler);
+        editDetectArea.addEventListener("click", this.setPointHandler);
         break;
 
       case "freeDrawing":
@@ -32,6 +35,7 @@ class EditLayer extends Component {
     }
   }
 
+
   buildLines() {
     if (this.state.lines.size > 0) {
       let xScale = this.props.xScale;
@@ -40,16 +44,28 @@ class EditLayer extends Component {
       let width = xRange[xRange.length-1];
       let height = this.props.yScale.range()[0];
 
-      let points = this.state.lines.toArray().map((line, idx)=>{
-        return line.map((point, idx)=>{
+      let clickedPoint = this.state.clickedPointIndex;
+
+      var that = this;
+
+      let points = this.state.lines.toArray().map((line, idx1)=> {
+        return line.map((point, idx2)=> {
           var x = xScale(point.x),
               y = yScale(point.y);
 
+          let clicked = clickedPoint && clickedPoint[0] === idx1 && clickedPoint[1] === idx2;
+
+          let mousemoveHandler = clicked ? that.circleUpdatePosition : null
+
           if (x > 0 && y > 0 && x < width && y < height) {
             return (<circle key={ `${point.x}, ${point.y}`}
-              r="3"
+              r={ clicked ? 5 : 3 }
               fill="transparent"
               stroke="red"
+              strokeWidth={ clicked ? 5 : 1}
+              data-line-index={ `${idx1},${idx2}` }
+              onClick={ that.toggleClickedCircle }
+              onMouseMove={ mousemoveHandler }
               cx={ xScale(point.x) }
               cy={ yScale(point.y) }/>)
           } else return null;
@@ -64,7 +80,7 @@ class EditLayer extends Component {
     } else return null
   }
 
-  drawLineHandler(evt) {
+  setPointHandler(evt) {
     // Push each pair of x, y coordinates
     let x = evt.offsetX;
     let y = evt.offsetY;
@@ -89,6 +105,44 @@ class EditLayer extends Component {
         lines: this.state.lines.push(currLine)
       });
     }
+  }
+
+  toggleClickedCircle(evt) {
+    var targetIdx = evt.target.dataset.lineIndex.split(",")
+                        .map(x=>parseInt(x, 10));
+
+    let currentClicked = this.state.clickedPointIndex;
+
+    if (currentClicked &&
+        currentClicked[0] === targetIdx[0] &&
+        currentClicked[1] === targetIdx[1]) {
+      this.setState({ clickedPointIndex: null })
+    } else {
+      this.setState({ clickedPointIndex: targetIdx })
+    }
+  }
+
+  circleUpdatePosition(evt) {
+    let parentRect = evt.target.parentElement.getBoundingClientRect();
+
+    let x = evt.clientX - parentRect.x;
+    let y = evt.clientY - parentRect.y;
+
+    let xInverted = this.props.xScale.invert(x);
+    let yInverted = this.props.yScale.invert(y);
+    var point = {
+      x: xInverted,
+      y: yInverted
+    };
+
+    let pointIdx = this.state.clickedPointIndex;
+    this.setState({
+      lines: this.state.lines.update(pointIdx[0],
+        (oldPoint) => {
+          oldPoint[pointIdx[1]] = point;
+          return oldPoint
+        })
+    })
   }
 
   freeDrawingHandler(evt) {
