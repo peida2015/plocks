071e3262f2ada418f60e8c092072b4e3a479acc3
PeiDa Kuo <guo.peida@gmail.com>
2017-08-09 21:19:27 -0700

    change Navbar toggle control

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Container } from 'flux/utils';
 import CurrentUserStore from '../stores/CurrentUserStore';
 import FirebaseStore from '../stores/FirebaseStore';
 import { Link } from 'react-router';
-import { Navbar as RBNavbar, Button, Collapse }  from 'react-bootstrap';
+import { Navbar as RBNavbar, Button, Collapse, Glyphicon }  from 'react-bootstrap';
 
 class Navbar extends Component {
   static getStores() {
@@ -21,52 +21,35 @@ class Navbar extends Component {
   constructor(props) {
     super(props);
     this.signOut = this.signOut.bind(this);
-    this.mousemoveHandler = this.mousemoveHandler.bind(this);
+    this.toggleShowNavbar = this.toggleShowNavbar.bind(this);
 
     this.state = {
-      lastUpdated: null,
       showNavbar: true
     };
   }
 
+  componentDidMount() {
+    window.setTimeout(()=>{
+      this.toggleShowNavbar();
+    }, 5000)
+  }
+
   componentDidUpdate(prevProps, prevState) {
-    if (window.location.pathname.match('stock')) {
-      document.addEventListener('mousemove', this.mousemoveHandler);
-    } else {
-      document.removeEventListener('mousemove', this.mousemoveHandler);
-      this.setState({ showNavbar: true });
-    }
   }
 
   componentWillUnmount() {
-    document.removeEventListener('mousemove', this.mousemoveHandler);
   }
 
   signOut() {
-    // Manually trigger navbar collapse
-    clearTimeout(this.collapseTimeout);
     document.querySelector('button.navbar-toggle').click();
     this.state.firebase.get("auth")().signOut();
   }
 
-  mousemoveHandler(evt) {
-    if (!this.state.lastUpdated) this.setState({ lastUpdated: Date.now() });
-    if (Date.now() - this.state.lastUpdated > 300) {
-      this.setState({ lastUpdated: Date.now() });
-      if (evt.clientY < 80) {
-        clearTimeout(this.collapseTimeout);
-        this.collapseTimeout = null;
-        this.setState({
-          showNavbar: true
-        });
-      } else if (!this.collapseTimeout){
-        this.collapseTimeout = setTimeout(()=>{
-          this.setState({
-            showNavbar: false
-          });
-        }, 8000);
-      }
-    }
+
+  toggleShowNavbar(evt) {
+    this.setState({
+      showNavbar: !this.state.showNavbar
+    })
   }
 
   render() {
@@ -84,33 +67,48 @@ class Navbar extends Component {
         </RBNavbar.Form>) : "";
 
     return (
+      <div>
       <Collapse transitionAppear={ true }
                 in={ this.state.showNavbar }
                 timeout={ 1000 }>
         <div>
                 {/*<!-- navbar begins -->*/}
-        <RBNavbar fixedTop={true} className="navBG">
-            <RBNavbar.Header>
-              <RBNavbar.Brand>
-                <Link to="/welcome">
-                  <img src={logo} className="icon" alt="Plocks logo"/>
-                </Link>
-                {" "}
-                <Link to="/welcome" className="page-scroll" >
-                  <span className="light title-font">PLOCKS</span>
-                </Link>
-              </RBNavbar.Brand>
-              <RBNavbar.Toggle />
-            </RBNavbar.Header>
-          {/*<!-- Collect the nav links, forms, and other content for toggling -->*/}
-            <RBNavbar.Collapse>
-              { showName }
-              { logoutButton }
-            </RBNavbar.Collapse>
-          {/*<!-- /.navbar-collapse -->*/}
-        </RBNavbar>
+          <RBNavbar fixedTop={true}
+                    className="navBG"
+                    onClick={ this.toggleShowNavbar }>
+              <RBNavbar.Header>
+                <RBNavbar.Brand>
+                  <Link to="/welcome">
+                    <img src={logo} className="icon" alt="Plocks logo"/>
+                  </Link>
+                  {" "}
+                  <Link to="/welcome" className="page-scroll" >
+                    <span className="light title-font">PLOCKS</span>
+                  </Link>
+                </RBNavbar.Brand>
+                <RBNavbar.Toggle />
+              </RBNavbar.Header>
+            {/*<!-- Collect the nav links, forms, and other content for toggling -->*/}
+              <RBNavbar.Collapse>
+                { showName }
+                { logoutButton }
+              </RBNavbar.Collapse>
+            {/*<!-- /.navbar-collapse -->*/}
+          </RBNavbar>
         </div>
       </Collapse>
+        <div>
+          <span className="centered-toggle-button"
+                onClick={ this.toggleShowNavbar } >
+            <Glyphicon glyph="chevron-down"
+                        style={ {
+                          textAlign: "center",
+                          display: "block",
+                          color: "#24a6e8"
+                          } } />
+          </span>
+        </div>
+      </div>
     )
   }
 }
